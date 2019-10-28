f73ac44391e11fade20429640fa99e8d1693c5e2
Peter Kerins <pete.kerins@gmail.com>
2019-10-28 09:45:50 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      task: 'Build todo App',
+      isCompleted: true,
+    },
+    {
+      task: 'Make it Pretty',
+      isCompleted: false,
+    },
+    {
+      task: 'Prevent being able to be completed if task is non existent',
+      isCompleted: true,
+    },
+  ])
+
+  // Function Run When Users hits enter inside todo
+  function handleKeyDown(e, i) {
+    if (e.key === 'Enter') {
+      createTodoAtIndex(e, i)
+    }
+    if (e.key === 'Backspace' && todos[i].task === '') {
+      e.preventDefault()
+      return removeTodoAtIndex(i)
+    }
+  }
+
+  function createTodoAtIndex(e, i) {
+    const newTodos = [...todos]
+    newTodos.splice(i + 1, 0, {
+      task: '',
+      isCompleted: false,
+    })
+    setTodos(newTodos)
+    setTimeout(() => {
+      document.forms[0].elements[i + 1].focus()
+    }, 0)
+  }
+
+  const updateTodoAtIndex = (e, i) => {
+    const newTodos = [...todos]
+    newTodos[i].task = e.target.value
+    setTodos(newTodos)
+  }
+
+  function removeTodoAtIndex(i) {
+    if (i === 0 && todos.length === 1) return
+    setTodos(todos =>
+      todos.slice(0, i).concat(todos.slice(i + 1, todos.length))
+    )
+    setTimeout(() => {
+      document.forms[0].elements[i - 1].focus()
+    })
+  }
+
+  function toggleTodoCompletedAtIndex(index) {
+    if (todos[index].task === '') return
+    const newTodos = [...todos]
+    newTodos[index].isCompleted = !newTodos[index].isCompleted
+    setTodos(newTodos)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="header">
+        <img className="logo" src={logo} alt="logo" />
       </header>
+      <form className="todo-list">
+        <ul>
+          {todos.map((todo, i) => (
+            <li
+              className={`todo ${
+                todo.isCompleted ? 'todo-is-completed' : 'task-incomplete'
+              }`}
+              key={i.toString()}
+            >
+              <div
+                className="checkbox"
+                onClick={() => toggleTodoCompletedAtIndex(i)}
+              >
+                {todo.isCompleted && <span>&#x2714;</span>}
+              </div>
+              <input
+                type="text"
+                value={todo.task}
+                onKeyDown={e => handleKeyDown(e, i)}
+                onChange={e => updateTodoAtIndex(e, i)}
+              />
+            </li>
+          ))}
+        </ul>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
